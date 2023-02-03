245760316fce351f04d7803b9b7b7127943da265
gopakistan <101689049+gopakistan@users.noreply.github.com>
2023-02-02 23:16:33 -0500

    Update Main.java

--- checkers/src/Main.java
+++ checkers/src/Main.java
@@ -20,19 +20,33 @@ public class Main {
         String dir = "help";
         cBoard b = cBoard.getInstance();
         b.createBoard(); //<--INIT B
-        //System.out.println("here:" + dir.substring(0,2) + "rrr");
+        b.displayBoard();
         while(!(dir.equals("end") )){
             if (dir.equals("help")) getHelp();
-            
             else {
+                b.displayBoard();
                 moveList = dir.split(" ", 6);
             
-                if (moveList[0].equals("move")){
+                if (moveList[0].toLowerCase().equals("move")){
                     System.out.println("piece to be moved: " + moveList[1]);
                     
                     for(i = 2; i < moveList.length; i++){
                         System.out.println(moveList[i]);
-                        if (moveList[i].length() == 2) verifySpotColour(moveList[i]);
+                        if (moveList[i].length() == 2){
+                            int xpos = char2num(moveList[1].charAt(0));
+                            int ypos = char2num(moveList[1].charAt(1));
+                            Piece p = b.getPiece(xpos, ypos);
+                            
+                            int x = char2num(moveList[i].charAt(0));
+                            int y = char2num(moveList[i].charAt(1));
+                            if (validateMoves(p, b, x, y) == true){
+                                p.movePiece(x, y);
+                            }
+                            
+                        } 
+                        else{
+                            printError("Invalid format followed.");
+                        }
                     }
                     
                 }
@@ -54,26 +68,56 @@ public class Main {
     
         public static int char2num(char c){
             int ascii = (int) c;
-            if(ascii > 96 && ascii < 105)
-                return ascii - 96;
-            if(ascii > 64 && ascii < 73)
-                return ascii - 64;
+            if(ascii > 96 && ascii < 105){
+                ascii -= 97;
+                return ascii;
+            }
+            else if(ascii > 64 && ascii < 73){
+                ascii -= 65;
+                return ascii;
+            }
+            else if(ascii > 48 && ascii < 58){
+                ascii -= 49;
+                return ascii;
+            }
             else return -1;
         }
         
-        public static boolean verifySpotColour(String move){
-            System.out.println("\t\t" + move);
-            int ycoords = move.charAt(1); 
-            int xcoords = char2num(move.charAt(0));
-            
-            //checks if the entered coordinates are on a black spot
-            if((xcoords + ycoords)%2 == 1) return false;
-            else return true;
-        }
-        //A
         public static String printError(String err){
             System.out.println("Error due to " + err);
             return "end";
+        }
+        
+        public static boolean validateMoves(Piece p, cBoard b, int x, int y){
+            //check if piece exists
+            if(p == null){
+                printError("Piece not found");
+                return false;
+            }
+            //check if piece belongs to player
+            
+            else{
+                int[] distance = p.getDistance(x, y);
+                int xdis = distance[0];
+                int ydis = distance[1];
+                
+                if((x + y)%2 != 0) return false;
+                
+                else if(p.getType() != b.getTurn()){
+                    printError("It is not your turn");
+                    return false;
+                }
+                //check if direction its going in is ok
+                else if(p.getPromoted() == false && p.getYDirection(y) != b.getTurn()){
+                    printError("Piece cannot move in this direction.");
+                    return false;
+                }
+            }
+//            else if (xdis != 1 || ydis != 1){
+//                printError("Piece cannot travel this far.");
+//                return false;
+//            }
+            return true; //CHECK IF THE MOVE SPOT IS ACTUALLY ON THE BOARD
         }
     
 }
