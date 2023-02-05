a365daa0ac475fce2c5908a3442e20a8320925f2
gopakistan <101689049+gopakistan@users.noreply.github.com>
2023-02-04 20:32:38 -0500

    Update Piece.java
    ended up removing this file completely, this is simply an archive of a previous version, left up for future reference

--- checkers/src/Piece.java
+++ checkers/src/Piece.java
@@ -1,68 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author vsatchi
- */
 public class Piece {
     int type;
     boolean promoted = false;
     int xpos;
     int ypos;
-    
+    static cBoard b = cBoard.getInstance();
     public Piece(int type, int xpos, int ypos){
-       this.type = type;
-       this.xpos = xpos;
-       this. ypos = ypos;
+        this.type = type;
+        this.xpos = xpos;
+        this. ypos = ypos;
     }
-    
+
     public int getXPos(){
         return xpos;
     }
-    
+
     public int getYPos(){
         return ypos;
     }
-    
+
     public void setXPos(int xpos){
         this.xpos = xpos;
     }
-    
+
     public void setYPos(int ypos){
         this.ypos = ypos;
     }
-    
+
     public int getType(){
         return this.type;
     }
-    
+
     public void setType(int type){
         this.type = type;
     }
-    
+
     public boolean getPromoted(){
         return promoted;
     }
-    
+
     public void setPromoted(boolean promoted){
         this.promoted = promoted;
     }
-    
+
     public int[] getDistance(int xcoords, int ycoords){
         int dx = xcoords - this.xpos;
         int dy = ycoords - this.ypos;
         return new int[]{dx,dy};
     }
-    
+
     public int getYDirection(int ycoords){
         if(ycoords > ypos) return 1;
         else return -1;
     }
-    
+
     public boolean movePiece(int xcoords, int ycoords){
         //RETURN TRUE IF PIECE IS KILLED, NOT MEANING SUCCESFUL MOVE
         //check if move is valid:
@@ -77,16 +67,15 @@ public class Piece {
         //          delete opposing piece from board
         //      not killing
         //          dest should be free spot
-        //          dest should be within (1, 1) from pos 
-        
+        //          dest should be within (1, 1) from pos
+
         //if move is succesful, do the move
         //  if killing happened:
         //      delete item from array or add "dead/alive" attribute to pieces
         //  if killing happen OR no happen:
-        //      change position value 
+        //      change position value
         //      change visual board
-        
-        cBoard b = cBoard.getInstance();
+
         Piece toKill = b.getPiece(xcoords, ycoords);
         if(toKill == null){
             b.displayPieces();
